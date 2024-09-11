423b6cef7dee7a080b48a241e54fe5222090c892
Елена Владимировна <50554917+Skylin44@users.noreply.github.com>
2024-09-11 12:54:56 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Anna' : 1980, 'Lena': 1988}
+print(my_dict)
+print(my_dict.get('Anna'))
+print(my_dict.get('Kata'))
+my_dict.update({'Wika': 2001, 'Rita': 1990})
+print(my_dict)
+my_dict.pop('Anna')
+print(my_dict)
+my_set_= {1,2,3,4, 'time', 4,3,2,1, 'time'}
+print(my_set_)
+my_set_.update({5,6})
+print(my_set_)
+my_set_.discard(2)
+print(my_set_)
